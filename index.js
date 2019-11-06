46bdb6e23560a2ae0c600f184a1703ea606bd01c
kipernicus <kip.armstrong@gmail.com>
2019-11-06 11:46:29 -0700

    More implementation

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ async function run () {
     const gameId = await createGame(players)
     // const state = await getGameState(gameId)
     const state = await joinGame(gameId)
-    console.log(JSON.stringify(state, null, 2))
     let gameOver = false
     let nextState = state
     do {
+      // console.log('STATE IS:', JSON.stringify(nextState, null, 2))
       nextState = await makeMove(gameId, nextState)
+      console.log('MADE A MOVE')
     } while (!gameOver)
   } else {
     console.log('ERROR: NOT DONE')
@@ -44,39 +45,145 @@ async function createGame (players) {
 async function makeMove (gameId, state) {
   const { data, turnToken } = state
   const move = determinePlacement(data)
-  return axios.post(`http://393d049b.ngrok.io/${gameId}/moves`, move, {
+  const resp = await axios.post(`http://393d049b.ngrok.io/${gameId}/moves`, move, {
     headers: {
       'x-turn-token': turnToken
     }
   })
+  const nextTurnToken = resp.headers['x-turn-token']
+  return { turnToken: nextTurnToken, data: resp.data }
 }
 
 function determinePlacement(data) {
-  const { current_piece, next_piece, board } = data
-
-  console.log(board)
-
-  switch (current_piece) {
-    case 'I':
-
-    case 'J':
-
-    case 'L':
-
-    case 'O':
-
-    case 'S':
-
-    case 'Z':
-
-    case 'T':
-  }
+  const { current_piece, next_piece, players  } = data
+  const { board } = players.find(p => p.name === 'KMILLER')
+  console.log('CURRENT STATE', current_piece, board)
+  const options = findPlacementOptions(current_piece, board)
 
   return {
-    locations: [
-      {}
-    ]
+    locations: options[0]
   }
+}
+
+function placeS (options, next_piece, board) {
+  return [
+    { row: 0, col: 0 },
+    { row: 0, col: 1 },
+    { row: 1, col: 1 },
+    { row: 1, col: 2 },
+  ]
+}
+
+function placeZ (options, next_piece, board) {
+  return [
+    { row: 1, col: 0 },
+    { row: 1, col: 1 },
+    { row: 0, col: 1 },
+    { row: 0, col: 2 },
+  ]
+}
+
+function placeL (options, next_piece, board) {
+  return [
+    { row: 2, col: 0 },
+    { row: 1, col: 0 },
+    { row: 0, col: 0 },
+    { row: 0, col: 1 },
+  ]
+}
+
+function placeO (options, next_piece, board) {
+  return [
+    { row: 0, col: 0 },
+    { row: 0, col: 1 },
+    { row: 1, col: 0 },
+    { row: 1, col: 1 },
+  ]
+}
+
+function placeT (options, next_piece, board) {
+  return [
+    { row: 0, col: 0 },
+    { row: 0, col: 1 },
+    { row: 0, col: 1 },
+    { row: 1, col: 1 },
+  ]
+}
+
+function placeI (options, next_piece, board) {
+  return [
+    { row: 0, col: 0 },
+    { row: 0, col: 1 },
+    { row: 0, col: 2 },
+    { row: 0, col: 3 },
+  ]
+}
+
+function placeJ (options, next_piece, board) {
+  return [
+    { row: 0, col: 0 },
+    { row: 0, col: 1 },
+    { row: 0, col: 2 },
+    { row: 1, col: 0 },
+  ]
+}
+
+function findPlacementOptions (current_piece, board) {
+
+  let positionFunc, placement
+  switch (current_piece) {
+    case 'S':
+      placement = placeS()
+      break;
+    case 'Z':
+      placement = placeZ()
+      break;
+    case 'L':
+      placement = placeL()
+      break;
+    case 'O':
+      positionFunc = oPositions
+      break;
+    case 'T':
+      placement = placeT()
+      break;
+    case 'I':
+      positionFunc = iPositions
+      break;
+    case 'J':
+      placement = placeJ()
+      break;
+    default: console.error('UNEXPECTED PIECE TYPE')
+  }
+  let validPlacements = []
+  if (!positionFunc) {
+    return [placement]
+  } else {
+    for (let y = 0; y < board.length; y++ ) {
+      const row = board[y]
+      for (let x = 0; x < row.length; x++) {
+        const isEmpty = board[y][x] === null
+        if (isEmpty) {
+          const options = positionFunc(x, y)
+          validPlacements = validPlacements.concat(options.filter(o => isValidPlacement(o, board)))
+        }
+      }
+    }
+    console.log('VALID PLACEMENTS ARE', validPlacements)
+    return validPlacements
+  }
+}
+
+function isValidPlacement(placement, board) {
+  let valid = true
+  for (let i = 0; i < placement.length; i++) {
+    const p = placement[i]
+    const {row, col} = p
+    if (row < 0 || col < 0 || row >= board.length || col >= board[0].length || board[row][col] !== null) {
+      valid = false
+    }
+  }
+  return valid
 }
 
 const iPositions = (row, col) => [
